f71c582faeb68b8c6e6702547f8ba58d2ad772da
Rewina Pakpahan <ifs19008@students.del.ac.id>
2021-11-25 20:26:17 +0700

    commit tugas 4

---
+++ tugas4/script.js
@@ -0,0 +1,13 @@
+var indikator = 9;
+
+if (indikator > 7) {
+    hasil = "Belajar";
+} else if (indikator == 12) {
+	hasil = "Makan";
+} else {
+	hasil = "Tidur";
+}
+
+console.log(hasil);
+
+console.log("karna indikator ku nilai nya " +indikator+" maka jadwal ku sekarang adalah "+hasil);
